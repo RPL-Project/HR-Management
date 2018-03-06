6e2f20b30224dbacab1557fe58c4cdc89a03e874
DimasPrana <dimas.prana73@gmail.com>
2018-03-06 21:12:55 +0700

    profile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -40,40 +40,13 @@
 									<tr>	
 										<td class="td">Division</td><td>: {{Auth::user()->division_id}}</td>
 									</tr>
-									<tr>	
+									<tr>
 										<td class="td">Grade</td><td>: {{Auth::user()->grade_id}}</td>
 									</tr>
 								</table>
 							</div>
 						</div>
 					</div>
-			<!--@if (count($users) > 0)				
-				@foreach ($users as $count => $user)
-					@if ($count % 3 == 0)
-						</div><div class="row">				
-					@endif
-					<div class="col-4" style="margin-bottom: 20px;">
-						<div class="card" style="width: 18rem;">
-							<!-- <img class="card-img-top" src="..." alt="Card image cap">
-							<div class="card-body">
-								<h5 class="card-title">{{$user->name}}</h5>
-							    <p class="card-text" >
-							    	{{$user->gender}}<br>
-							    	{{$user->email}}				    	
-							    </p>
-							    {!! Form::open(['route' => ['profile.edit', $user->id], 'method' => 'GET']) !!}
-								{{csrf_field()}}
-								{{ Form::submit('Edit', ['class' => 'btn btn-warning']) }}
-								{!! Form::close() !!}
-							</div>
-						</div>			
-					</div>					
-				@endforeach				
-			</div>		
-			@else
-				<div>Tidak ada user</div>
-			@endif
-			-->
 		</div>
 	</div>
 @endsection
